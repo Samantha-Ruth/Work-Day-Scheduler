7a3edcc1639ca02b91a92783017d642d55d525c9
Samantha-Ruth <sam.haberman@gmail.com>
2022-05-17 10:35:16 -0600

    styling almost finished

---
+++ Develop/script.js
@@ -0,0 +1,11 @@
+
+// $(".time-block").selectable({
+//     connectWith: $(".time-block")
+//   });
+
+// $(".row .time-block").sortable({
+  //  connectWith: $(".row .time-block")
+  //});
+  
+// Display current day
+  $("#currentDay").text(moment().format('dddd, MMM Do'));
